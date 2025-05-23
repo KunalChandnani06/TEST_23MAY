d98032677dad51c51b278aac6049ed4979bd66b7
Kunal S Chandnani <kunal.chandnani@s.amity.edu>
2025-05-23 11:15:04 +0530

    I changes it to addition of 5 numbers

--- addition.c
+++ addition.c
@@ -1,11 +1,13 @@
 include<stdio.h>
 void main()
 {
-	int a,b,c;
+	int a,b,c,d,e,f;
 		a=5;
 		b=6;
 		c=8;
-		d= a+b+c;
+		e=5;
+		f=8;
+		d= a+b+c+e+f;
 		printf("The addtion of two numbers: ",d)
 			return 0;
 }
