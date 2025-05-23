b168e1a54a5aa8c696f5e8f8aa3689b87f752787
Kunal S Chandnani <kunal.chandnani@s.amity.edu>
2025-05-23 11:21:40 +0530

    Multiplication

--- addition.c
+++ addition.c
@@ -1,11 +1,15 @@
 include<stdio.h>
 void main()
 {
-	int a,b,c;
+	int a,b,c,d,e,f;
 		a=5;
 		b=6;
 		c=8;
-		d= a+b+c;
+		e=5;
+		f=8;
+		d= a+b+c+e+f;
+		h=a*b*c*d*e*f;
 		printf("The addtion of two numbers: ",d)
+			printf("The ultiplication of 2 numbers",h)
 			return 0;
 }
