242383fb78d0af0de04c31e78720efc5449d566d
Kunal S Chandnani <kunal.chandnani@s.amity.edu>
2025-05-23 11:18:41 +0530

    Done substraction of 5 numbers

--- addition.c
+++ addition.c
@@ -1,11 +1,15 @@
 include<stdio.h>
 void main()
 {
-	int a,b,c;
+	int a,b,c,d,e,f;
 		a=5;
 		b=6;
 		c=8;
-		d= a+b+c;
-		printf("The addtion of two numbers: ",d)
+		e=5;
+		f=8;
+		d= a+b+c+e+f;
+		g=a-b-c-e-f;
+		printf("The addtion of numbers: ",d)
+			printf("The substraction of the numbers",g)
 			return 0;
 }
